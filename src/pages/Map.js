accd4b98d29a70017e0a09fc7e5ec89ba7109d05
aleza89 <alessandro.zaccolo@bts.tech>
2019-09-26 11:25:46 +0700

    create map page

---
+++ src/pages/Map.js
@@ -0,0 +1,17 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonText,  IonTabs, IonTabBar, IonTabButton, IonBadge, IonIcon, IonLabel, IonButton, IonInput, IonItem} from '@ionic/react';
+import React from 'react';
+import '../theme/page.css';
+
+class Map extends React.Component {
+
+	render() {
+		return (
+	    <IonPage>
+	      <IonContent className="page">
+				
+				</IonContent>
+	    </IonPage>
+	  );
+	}
+
+export default Map;
