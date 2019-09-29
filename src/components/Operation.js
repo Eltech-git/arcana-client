3ec3770c0c48b775937cf7b406efe68ffb60f6b0
aleza89 <alessandro.zaccolo@bts.tech>
2019-09-29 10:38:50 +0700

    working on adding comments to operation

--- src/components/Operation.js
+++ src/components/Operation.js
@@ -1,22 +1,35 @@
-import {IonGrid, IonRow, IonCol, IonText, IonCard, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonButton, IonLabel, IonIcon, IonCardHeader, IonImg, IonAvatar, IonChip } from '@ionic/react';
+import {IonGrid,IonText, IonCard, IonImg, IonAvatar, IonCardContent, IonButton, IonItem} from '@ionic/react';
 import React from 'react';
-import '../theme/card.css'
+import SmallComment from './SmallComment';
+import '../theme/card.css';
+import '../theme/comment.css';
 
 class Operation extends React.Component {
 
-	state= {
+	state = {
 		header: {
 			src: '../images/woman.jpg',
 			agent: 'Sempronio',
 			cases: 4
-		}
+		},
+		comments: false,
 	}
+
+	showComments = (event) => {
+		let comments = this.state.comments
+		let changed = !comments
+
+		this.setState({
+			comments: changed
+		})
+	}
+
+
 
 	render() {
 		return (
 			<IonCard className="card">
-
-			<IonGrid className="grid">
+			<IonGrid className="grid-operation" onClick={() => this.props.goToDetail()}>
 			<IonAvatar className="avatar">
 				<img className="img" src={this.state.header.src}/>
 			</IonAvatar>
@@ -24,10 +37,12 @@ class Operation extends React.Component {
 			<IonText className="text">23/09</IonText>
 			</IonGrid>
 
-		<IonCardContent>
-			Trovato l'amante
-		</IonCardContent>
-
+		<IonText className="display" onClick={this.showComments}>{this.state.comments === true ? "Nascondi commenti" : "Visualizza commenti"}</IonText>
+		<IonGrid className={this.state.comments === true ? "" : "hidden"}>
+			<SmallComment/>
+			<SmallComment/>
+			<SmallComment/>
+		</IonGrid>
 	</IonCard>
 
 
