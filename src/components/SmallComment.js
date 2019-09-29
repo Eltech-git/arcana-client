94f4eea0bb30a306c5e2a1c8c3c75d79ed6f9e61
aleza89 <alessandro.zaccolo@bts.tech>
2019-09-29 10:39:28 +0700

    Create comment component to be shown in operation component

---
+++ src/components/SmallComment.js
@@ -0,0 +1,27 @@
+import { IonContent, IonCard, IonCardHeader, IonRow, IonCol, IonText,IonCardSubtitle, IonCardTitle, IonIcon, IonLabel, IonButton, IonCardContent, IonItem, IonAvatar, IonGrid, IonTitle} from '@ionic/react';
+import React from 'react';
+import '../theme/comment.css';
+
+class SmallComment extends React.Component {
+
+	state= {
+		comment: {
+			image: '../images/lover.jpg'
+		}
+	}
+
+	render() {
+		return (
+			<IonItem className="comment">
+			<IonGrid className="grid-comment">
+			<IonAvatar className="avatar">
+				<img className="img" src={this.state.comment.image}/>
+			</IonAvatar>
+			<IonTitle className="text">Trovato l'amante</IonTitle>
+			</IonGrid>
+			</IonItem>
+	  );
+	}
+};
+
+export default SmallComment;
